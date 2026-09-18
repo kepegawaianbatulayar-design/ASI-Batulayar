e360a815aefa14dcbecc16fdb028607c159e95d3
kepegawaianbatulayar-design <kepegawaianbatulayar@gmail.com>
2026-09-18 10:37:13 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/asibatulayar/app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.asibatulayar.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        // URL website Anda
+        webView.loadUrl("https://asibatulayar01.infinityfreeapp.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
